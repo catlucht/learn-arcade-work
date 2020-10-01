f31c5e0e34c4a27f58e0c145ba283ac5ba40aa40
Cat Lucht <cat.lucht@my.simpson.edu>
2020-09-30 20:04:17 -0500

    Work on lab 5

--- Testing/test.py
+++ Testing/test.py
@@ -1,6 +1,80 @@
-class Dog():
+""" Sprite Sample Program """
+
+import random
+import arcade
+
+# --- Constants ---
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.2
+COIN_COUNT = 50
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+
+
+class MyGame(arcade.Window):
+    """ Our custom Window Class"""
+
     def __init__(self):
-        self.age = 0
-        self.name = ""
-        self.weight = 0
-        print("A new dog!")
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
+
+        self.player_list = None
+        self.coin_list = None
+
+        self.player_sprite = None
+        self.score = 0
+
+        self.set_mouse_visible(False)
+        arcade.set_background_color(arcade.color.PEAR)
+
+    def setup(self):
+
+        # Sprite Lists
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        self.score = 0
+
+        self.player_sprite = arcade.SpriteList("Character.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 100
+        self.player_sprite.center_y = 300
+        self.player_list.append(self.player_sprite)
+
+        for i in range(COIN_COUNT):
+        coin.player_sprite = arcade.SpriteList("coin.png", SPRITE_SCALING_COIN)
+        coin.player_sprite.center_x = random.randrange(SCREEN_WIDTH)
+        coin.player_sprite.center_y = random.randrange(SCREEN_HEIGHT)
+        self.coin_list.append(coin)
+
+
+    def on_draw(self):
+        arcade.start_render()
+        self.player_list.draw()
+        self.coin_list.draw()
+        output = "Score: " + str(self.score)
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 24)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def on_update(self, delta_time):
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+
+def main():
+    """ Main method """
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+
+
+    main()
